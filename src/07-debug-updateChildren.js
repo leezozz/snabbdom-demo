39d2e135cc57a3ed1266aa8e46ce98fc3b6e06ad
gaofuhong <157297759@qq.com>
2023-01-29 15:01:37 +0800

    feat: add debug updateChildren key

---
+++ src/07-debug-updateChildren.js
@@ -0,0 +1,23 @@
+import { init } from 'snabbdom/build/package/init'
+import { h } from 'snabbdom/build/package/h'
+
+const patch = init([])
+
+// 首次渲染
+let vnode = h('ul', [
+  h('li', '首页'),
+  h('li', '视频'),
+  h('li', '微博'),
+])
+
+let app = document.querySelector('#app')
+let oldVnode = patch(app, vnode)
+
+// updateChildren的执行过程
+vnode = h('ul', [
+  h('li', '首页'),
+  h('li', '微博'),
+  h('li', '视频'),
+])
+
+patch(oldVnode, vnode)
